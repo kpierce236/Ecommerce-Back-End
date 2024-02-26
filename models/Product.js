79a2af0b15be12b1ea9d67380e61ef3855ad87a7
Kevin Pierce <kpierce236@gmail.com>
2024-02-25 19:47:39 -0500

    added columns for Product and Category models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    price: {
+      type: Sequelize.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'category', // Assuming your Category model is named 'Category'
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
